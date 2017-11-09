5d2dac3128fec890e887c0834622e41d21752fbd
Sanath Bharadwaj <sanath.bharadwaj21@gmail.com>
2017-11-09 18:33:21 +0000

    MapsActivity.java edited online with Bitbucket

--- app/src/main/java/trucker/harsha/com/trucker/MapsActivity.java
+++ app/src/main/java/trucker/harsha/com/trucker/MapsActivity.java
@@ -50,7 +50,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
+        //myUpdate
 
     }
 
