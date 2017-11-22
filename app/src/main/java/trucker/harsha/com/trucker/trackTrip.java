9ae81e862b72ce351f39918a0f7e89fefd2c44be
melsuhas <suhasmelmuri@gmail.com>
2017-11-22 23:04:23 +0530

    completed with tracking activity.

---
+++ app/src/main/java/trucker/harsha/com/trucker/trackTrip.java
@@ -0,0 +1,13 @@
+package trucker.harsha.com.trucker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class trackTrip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_track_trip);
+    }
+}
