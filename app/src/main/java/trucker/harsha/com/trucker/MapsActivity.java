b91d7055dfeaa8e44950163575aba53fd49241a9
cd healthyTOD <sanath.bharadwaj21@gmail.com>
2017-12-03 20:37:12 +0530

    Registration activity added. OnCameraIdle bug fixed.

--- app/src/main/java/trucker/harsha/com/trucker/MapsActivity.java
+++ app/src/main/java/trucker/harsha/com/trucker/MapsActivity.java
@@ -91,20 +91,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mMap.clear();
             mMap.addMarker(new MarkerOptions().position(latlng).title("Marker"));
             mMap.animateCamera(CameraUpdateFactory.newLatLng(latlng));
+            //TODO: initialize location with lastKnownLocation if no GPS enabled
+            //TODO: alert user if gps disabled
         }
 
 
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -144,13 +137,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         };
         if (Build.VERSION.SDK_INT < 23) {
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
                 return;
             }
             startLocation();
@@ -161,11 +147,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 startLocation();
             }
         }
-
-        // Add a marker in Sydney and move the camera
-        // LatLng sydney = new LatLng(-34, 151);
-        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     void updateLocation(Location location) {
@@ -222,13 +203,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
                 return;
             }
             startLocation();
@@ -283,6 +257,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 showToast("Camera now idle");
             }
         };
+
+        mMap.setOnCameraIdleListener(onCameraIdleListener);
     }
 
     void showToast(String message) {
---
+++ app/src/main/java/trucker/harsha/com/trucker/RegistrationActivity.java
@@ -0,0 +1,74 @@
+package trucker.harsha.com.trucker;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+    final String USER_REGISTERED = "isRegistered";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        preferences = getSharedPreferences("com.harsha.trucker",MODE_PRIVATE);
+        if(preferences.getBoolean(USER_REGISTERED,false))
+        {
+            loadToMapsActivity();
+        }
+    }
+
+    public void onSubmit(View view)
+    {
+        ParseUser user = new ParseUser();
+        user.setUsername(getEditText(R.id.username).getText().toString());
+        user.setPassword(getEditText(R.id.password).getText().toString());
+        user.setEmail(getEditText(R.id.email).getText().toString());
+        user.put("phone", getEditText(R.id.phone_number).getText().toString());
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e!=null)
+                {
+                    showToast("Registration error");
+                    return;
+                }
+                showToast("Registration successful");
+                editor = preferences.edit();
+                editor.putBoolean(USER_REGISTERED, true);
+                editor.apply();
+                loadToMapsActivity();
+
+            }
+        });
+    }
+
+    void loadToMapsActivity()
+    {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
+    EditText getEditText(int id)
+    {
+        return (EditText)findViewById(id);
+    }
+
+    void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+}
